7edd233b88391b6e8d9202b00cab144815f709a3
Joanna Waczyńska <joanna.waczynska@orange.com>
2023-05-08 11:16:55 +0200

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# 
+Summmer events
+
+### Setup env
+```bash
+$ python3.9 -m venv .venv
+$ source .venv/bin/activate
+$ pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="aggregation_mod",
+    version="0.0.1",
+    description="Models to create events summary.",
+    author="kithogue, waczyjoan",
+    packages=find_packages(),
+)
