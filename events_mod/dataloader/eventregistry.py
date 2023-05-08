bf2c9e580e5fd40cd0071dcafaf88a9a546eb7bb
Joanna Waczyńska <joanna.waczynska@orange.com>
2023-05-08 12:26:02 +0200

    implement configs and add download data

--- events_mod/dataloader/eventregistry.py
+++ events_mod/dataloader/eventregistry.py
@@ -16,3 +16,35 @@ def newest_data(
         maxItems=max_items
     ):
         print(art)
+
+
+def recently_added_data(
+    topic: str,
+    eventregistry: EventRegistry
+):
+    """Print a list of recently added articles mentioning the topic"""
+    q = QueryArticlesIter(
+        conceptUri=eventregistry.getConceptUri(topic)
+    )
+    for art in q.execQuery(
+            eventregistry, sortBy = "date"
+    ):
+        print(art)
+
+
+def latest_events(
+    topic: str,
+    eventregistry: EventRegistry,
+    n_items: int,
+):
+    """Search for latest events related to the topic"""
+    q = QueryEvents(
+        conceptUri=eventregistry.getConceptUri(topic)
+    )
+    q.setRequestedResult(
+        RequestEventsInfo(
+            sortBy="date",
+            count=n_items
+        )
+    )
+    print(eventregistry.execQuery(q))
