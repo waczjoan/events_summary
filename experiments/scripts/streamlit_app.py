36417eb75ba8ab500e85dabde464f39207dfa2f7
Joanna Waczyńska <joanna.waczynska@orange.com>
2023-05-10 11:38:39 +0200

    implement annoted text

--- experiments/scripts/streamlit_app.py
+++ experiments/scripts/streamlit_app.py
@@ -4,11 +4,41 @@ Run: streamlit run streamlit_app.py.
 """
 from pathlib import Path
 
+from annotated_text import annotated_text
 import click
 import streamlit as st
 import yaml
 
+from events_mod.evaluate.metrics import calc_rouge
 from events_mod.utils import load_model_from_config
+
+
+def rgb_to_hex(r, g, b):
+    """Convert RGB to HEX."""
+    return '#{:02x}{:02x}{:02x}'.format(r, g, b)
+
+
+def calc_rouge_metrics(
+    summary_text: str,
+    text: str,
+    metric: str = 'rougeLsum'
+):
+    """Calculate rouge metrics and create annotated text."""
+    body = []
+    sentences = text.split(".")
+    for sentence in sentences:
+        score = calc_rouge(
+            summary_text, sentence, metrics=[metric],
+        )
+        precision = round(score[metric].precision, 2)
+        color = rgb_to_hex(
+            round(255 * (1 - precision)),
+            255,
+            round(255 * (1 - precision)),
+        )
+        body.append((sentence, f'{metric}.precision={precision}', color))
+
+    annotated_text(body)
 
 
 @click.command()
@@ -44,7 +74,7 @@ def main(
         text1 = st.text_area('First text', '... input 1st text')
         st.write('1st text len:', len(text1))
         if st.checkbox("Summary 1:"):
-            st.write('bullet_point_summary 1 #TODO')
+            st.write('bullet_point_summary 2 #TODO')
 
     with col2:
         text2 = st.text_area('2nd text', '... input 2nd text')
@@ -69,7 +99,8 @@ def main(
     col1, col2, col3 = st.columns(3)
 
     with col1:
-        if st.checkbox("Summary:"):
+        checkbox1 = st.checkbox("Summary:")
+        if checkbox1:
 
             text = text1
             input_ids = model.tokenize(text)
@@ -104,6 +135,33 @@ def main(
             generated_ids = model_key_phrase_summary.generate(input_ids)
             decoded_output3 = model_key_phrase_summary.decode(generated_ids)
             st.write(decoded_output3)
+
+    if checkbox1:
+        col1, col2, col3, col4 = st.columns(4)
+
+        with col1:
+            calc_rouge_metrics(
+                decoded_output1,
+                text1,
+            )
+
+        with col2:
+            calc_rouge_metrics(
+                decoded_output1,
+                text2,
+            )
+
+        with col3:
+            calc_rouge_metrics(
+                decoded_output1,
+                text3,
+            )
+
+        with col4:
+            calc_rouge_metrics(
+                decoded_output1,
+                text3,
+            )
 
 
 if __name__ == "__main__":
