abdee75906b3a6c1773d0dd2d81b306562f8e3b0
kithogue <kithogue.kinnat@gmail.com>
2023-05-10 11:00:24 +0200

    Add text truncation function

--- events_mod/utils.py
+++ events_mod/utils.py
@@ -1,5 +1,9 @@
 """Utility functions."""
 import importlib
+from typing import List
+from transformers import AutoTokenizer
+
+MAX_LENGTH = 127
 
 
 def load_model_from_config(cfg: dict):
@@ -12,3 +16,30 @@ def load_model_from_config(cfg: dict):
     )
 
     return cls(**cfg["kwargs"])
+
+
+def truncate_texts(texts_batch: List[str], add_eos_token: bool = True) -> str:
+    """Truncates text to max length of the model with or without a sep token (set the add EOS token parameter to True)
+    Receiving a batch of 4 texts, truncates the length of the input to 512 subword tokens as encoded by T5 tokenizer
+    Returns a concatenated string with or without separators
+    """
+    tokenizer = AutoTokenizer.from_pretrained('t5-base')
+    result = []
+    if add_eos_token:
+        for t in texts_batch:
+            t = tokenizer.encode(t)
+            if len(t) > MAX_LENGTH:
+                t = t[:-(len(t) - MAX_LENGTH)]
+            decoded = tokenizer.decode(t)
+            result.append(decoded)
+            result.append("</s>")
+        result = ''.join(result[:-1])
+    else:
+        for t in texts_batch:
+            t = tokenizer.encode(t)
+            if len(t) > MAX_LENGTH:
+                t = t[:-(len(t) - MAX_LENGTH)]
+            decoded = tokenizer.decode(t)
+            result.append(decoded)
+        result = ' '.join(result)
+    return result
